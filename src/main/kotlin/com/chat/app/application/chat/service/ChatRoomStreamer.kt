3d163efd6f6da2d3bad383583fcd19be55b191f7
ghdcksgml1 <ghdcksgml2@naver.com>
2024-09-07 14:40:46 +0900

    refact: sharedFlow readOnly로 변경

--- src/main/kotlin/com/chat/app/application/chat/service/ChatRoomStreamer.kt
+++ src/main/kotlin/com/chat/app/application/chat/service/ChatRoomStreamer.kt
@@ -1,6 +1,7 @@
 package com.chat.app.application.chat.service
 
 import com.chat.app.domain.message.dto.ChatMessage
+import com.chat.app.domain.room.exception.ChatRoomNotFoundException
 import kotlinx.coroutines.flow.*
 import java.util.concurrent.ConcurrentHashMap
 
@@ -9,9 +10,11 @@ object ChatRoomStreamer {
 
     fun getChatRoomStream(chatRoomId: String): Flow<ChatMessage> {
         return chatRoomStream.getOrPut(chatRoomId) { MutableSharedFlow() }
+            .asSharedFlow()
     }
 
     suspend fun sendMessage(chatRoomId: String, chatMessage: ChatMessage) {
         chatRoomStream[chatRoomId]?.emit(chatMessage)
+            ?: throw ChatRoomNotFoundException("메시지를 전송할 채팅방이 존재하지 않습니다. chatRoomId = ${chatRoomId}")
     }
 }
